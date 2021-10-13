a67c875f45a490d445ff178d9e3a9c70a5ffa5b1
Jaydeep Kumar Silawat <70216818+jayyuuuu@users.noreply.github.com>
2021-10-13 23:30:25 +0530

    Create polymorphism.js

---
+++ polymorphism.js
@@ -0,0 +1,33 @@
+// AUTHOR: jayyuuuu
+// JavaScript Concept: Polymorphism
+// GITHUB: https://github.com/jayyuuuu
+
+
+
+
+//Polymorphism using constructor function Employee which overrides another function
+
+var employee = new Employee('ethan winters');
+
+//below method will return Uppercase
+
+Employee.prototype.getDetails = function()
+{
+  return this.name.toUpperCase();
+}
+
+console.log(employee.getDetails()); //here output will be "ETHAN WINTERS"
+
+//Object and _prototype_ function
+
+function Employee(name)
+{
+  this.name = name;
+}
+
+Employee.prototype.getDetails = function()
+{
+  return this.name;
+}
+
+console.log(employee.getDetails()); //here output will be "ethan winters"
